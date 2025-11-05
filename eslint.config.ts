badecf1dcd2d5df9905d23be5eca3c2f5f450543
Vlad Zahiu <vladzahiu28@gmail.com>
2025-11-05 11:57:37 +0200

    Add linting, formatting and pre-commit hook

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig(
+  globalIgnores(["dist/**"]),
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_"
+        }
+      ]
+    },
+
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.config.*"]
+        }
+      }
+    }
+  }
+);
